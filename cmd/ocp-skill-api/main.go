4e19fded82f8a50cb868616c5c90e02726e83149
Petr Basov <pbasov@wallarm.com>
2021-07-27 18:45:34 +0300

    Fixes after review

--- cmd/ocp-skill-api/main.go
+++ cmd/ocp-skill-api/main.go
@@ -1,19 +1,27 @@
 package main
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"os"
 )
 
 func main() {
 	fmt.Println("This is Skill API")
+}
 
-	readConfig := func(path string) error {
+func ReadConfig(paths []string) ([]string, error) {
+	if len(paths) < 1 {
+		return nil, errors.New("empty config list")
+	}
+
+	readFile := func(path string) (string, error) {
 		file, error := os.Open(path)
+
 		if error != nil {
 			fmt.Println("can't open file")
 		}
-
 		defer func() {
 			if closeErr := file.Close(); closeErr != nil {
 				fmt.Println("can't close file")
@@ -21,12 +29,26 @@ func main() {
 			}
 		}()
 
-		return error
+		data := new(bytes.Buffer)
+
+		if _, error = data.ReadFrom(file); error != nil {
+			return "", error
+		}
+
+		return string(data.Bytes()), nil
 	}
 
-	for i := 0; i < 10; i++ {
-		if error := readConfig("config"); error != nil {
-			fmt.Println(error)
+	output := make([]string, 0)
+
+	for _, path := range paths {
+		data, error := readFile(path)
+
+		if error != nil {
+			fmt.Sprintf("problems with %v, skip", path)
+		} else {
+			output = append(output, data)
 		}
 	}
+
+	return output, nil
 }
